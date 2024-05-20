572d27e8419f20dbaffdb8a3c5a89b65d5fd57e7
Arayamariano <arayamariano67@gmail.com>
2024-05-20 09:25:01 -0300

    Ejercicios de Javascript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -7,12 +7,27 @@
  */
 function convertirgr(id) {
     if (id == "grados") {
-        let gra = document.getElementById("grados").value;
-        let grad = grad * Math.PI / 180;
-        document.getElementById("radianes").value;
+        let grad = document.getElementById("grados").value;
+        let rad = grad * Math.PI / 180;
+        document.getElementById("radianes").value=rad;
     } else if (id == "radianes") {
         let rad = document.getElementById("radianes").value;
-        let rad = rad * 180 / Math.PI;
-        document.getElementById("grados").value;
+        let grad = rad * 180 / Math.PI;
+        document.getElementById("grados").value=grad;
     }
+}
+
+let mostrarOcultar = (valorMO) => {
+    if (valorMO == "val_mostrar") {
+        document.getElementById("divMO").style.display = block;
+    } else if (valorMO == "val_ocultar") {
+        document.getElementById("divMO").style.display = none;
+    }
+}
+
+let sumar = () => {
+    let num1, num2;
+    num1 = Number(document.getElementsByName("sum_num1")[0].value);
+    num2 = Number (document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].value = num1 + num2;
 }
