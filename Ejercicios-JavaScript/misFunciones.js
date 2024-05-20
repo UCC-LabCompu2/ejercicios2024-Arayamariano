a30c3617e33cbf7a10d615a12a07b7cd0f033c54
Arayamariano <arayamariano67@gmail.com>
2024-05-20 08:44:39 -0300

    Se realizaron ejercicios en Javascript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,14 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function convertirgr(id) {
+    if (id == "grados") {
+        let gra = document.getElementById("grados").value;
+        let grad = grad * Math.PI / 180;
+        document.getElementById("radianes").value;
+    } else if (id == "radianes") {
+        let rad = document.getElementById("radianes").value;
+        let rad = rad * 180 / Math.PI;
+        document.getElementById("grados").value;
+    }
+}
