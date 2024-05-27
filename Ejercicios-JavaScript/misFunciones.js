ab721315abfa45efc69338630f0dbaa75f708efc
Arayamariano <arayamariano67@gmail.com>
2024-05-27 10:38:19 -0300

    Ejercicios JS 27/05

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -37,16 +37,70 @@ let sumar = () => {
 }
 
 let pasarPagina = () => {
-    let cant,unid,urlcomp;
-    cant= document.getElementById("distancia").value;
-    unid= document.getElementsByName("unidades")[0].value;
+    let cant, unid, urlcomp;
+    cant = document.getElementById("distancia").value;
+    unid = document.getElementsByName("unidades")[0].value;
     console.log(cant)
     console.log(unid)
-    urlcomp="segundaweb.html#" + cant + "#" +unid;
+    urlcomp = "segundaweb.html#" + cant + "#" + unid;
     window.open(urlcomp);
 
 }
 
 let cargarValores = () => {
     let cant
+}
+
+function dibujarCirculoCuadrado() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    const xmax = canvas.width;
+    const ymax = canvas.height;
+    ctx.fillStyle = "#ff0bb2";
+    const lado = 100;
+    const margen = 10;
+    ctx.fillRect(0 + margen, ymax - lado - margen, lado, lado);
+
+    ctx.fillStyle = "rgba(76,175,80,0.99)";
+    ctx.arc(xmax / 2, ymax / 2, 50, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+/**
+ * Dinuja una linea en la posicion que determina el usuario con el mouse
+ * @method cargarlisteners
+ */
+function cargarlisteners() {
+    document.getElementById("lienzo").addEventListener("mousemove", dibujar);
+}
+
+var bandera;
+
+function dibujar(event) {
+    const canvas = document.getElementById("lienzo");
+    const ctx = canva.getContext("2d");
+
+    let posx = event.clientX;
+    let posy = event.clientY;
+    console.log(posx, posy);
+
+    canvas.onmousedown = function () {
+        bandera = true
+    };
+    canvas.onmouseup = function () {
+        bandera = false
+    };
+
+    ctx.fillStyle = "rgba(76,175,80,0.99)";
+    if (bandera) {
+        ctx.fillRect(posx, posy, 5, 5);
+    }
+
+
+}
+
+function limpiarCanvas() {
+    const canvas = document.getElementById("lienzo");
+    canvas.width = canvas.width;
 }
